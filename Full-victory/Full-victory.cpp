bab4f8f0476a932d08b95b022fb1cc21eca4a94f
AlinaRinn <nik17042001@rambler.ru>
2022-12-04 20:15:13 +0300

    Initial setup

--- Full-victory/Full-victory.cpp
+++ Full-victory/Full-victory.cpp
@@ -1,13 +1,55 @@
 ﻿// 14. Полная победа. Армия сопротивления захватывает города (всего надо захватить 20 городов).
 // После захвата каждого города во все города, принадлежащие армии сопротивления, рассылается часть
-// секретного шифра, найденная во вновь захваченном городе, на что самый первый захваченный город
-// присылает во все города новую часть шифра. Необходимо получить в каждом городе полный шифр.
+// секретного шифра, найденная во вновь захваченном городе, через столицу (первый город). 
+// Необходимо получить в каждом городе полный шифр.
 
 #include <iostream>
 #include <mpi.h>
+#include <vector>
+
+const int CITY_AMOUNT = 20;
+
+class City {
+private:
+    char code[6];
+    char character;
+
+public:
+    City() {        
+        for (int i = 0; i < sizeof(code); i++)
+        {
+            character = 97 + rand() % 25;
+            code[i] = character;
+        }
+    }
+    ~City() { }
+
+    char* get_code() {
+        return code;
+    }
+    void recieve_code() {
+
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    // start
+    srand(time(0));
+    std::vector<City*> sities;
+
+    for (int i = 0; i < CITY_AMOUNT; i++)
+    {
+        sities.push_back(new City());
+    }
+
+    for (int j = 0; j < CITY_AMOUNT; j++)
+    {
+        for (int i = 0; i < sizeof(sities[j]->get_code()) - 2; i++)
+        {
+            std::cout << sities[j]->get_code()[i];
+        }
+        std::cout << "\n\n";
+    }
+
 }
+
