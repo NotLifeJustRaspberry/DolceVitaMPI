b986b7cd9ef9f96b6bf4766d0cb79d03b3c7bb5e
AlinaRinn <nik17042001@rambler.ru>
2022-12-24 03:22:28 +0300

    done?

--- Full-victory/Full-victory.cpp
+++ Full-victory/Full-victory.cpp
@@ -5,38 +5,29 @@
 
 #include <iostream>
 #include <mpi.h>
-
-const int CITY_AMOUNT = 20;
+#include <windows.h>
 
 int main(int argc, char** argv) {
-	int rank, size, len;
-	char proc_name[MPI_MAX_PROCESSOR_NAME];
-	MPI_Status status;
-	srand((unsigned int)time(0));
-	char sender[5] = { 97 + rand() % 25, 97 + rand() % 25 , 97 + rand() % 25 , 97 + rand() % 25 , 97 + rand() % 25 };
+	
+	int rank, size;
 
     MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
-	printf("Proc name %s, Rank %d, Size %d\n\n", proc_name, rank, size);
+	srand((unsigned int)time(0) * rank);
 
-	if (rank == 0) {
-		char crypt[100] = "";
-		MPI_Gather(&sender, sizeof(sender), MPI_CHAR, &crypt, sizeof(sender), MPI_CHAR, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&crypt, sizeof(crypt), MPI_CHAR, 0, MPI_COMM_WORLD);
-		printf("Crypt %s\n\n", crypt);
-		}
+	int crypt_shard = rand();
+	int* crypt;
+	crypt = (int*)malloc(size * sizeof(int));
 
-	if (rank > 0 && rank <= 20) {
-		//char sender[5] = { 97 + rand() % 25, 97 + rand() % 25 , 97 + rand() % 25 , 97 + rand() % 25 , 97 + rand() % 25 };
-		char recv_crypt[100];
-		MPI_Send(&sender, sizeof(sender), MPI_CHAR, 0, 5, MPI_COMM_WORLD);
-		printf("Loc Crypt %s\n\n", sender);
-		MPI_Recv(&recv_crypt, 100, MPI_CHAR, 0, 5, MPI_COMM_WORLD, &status);
-		printf("Recv Crypt %s\n\n", recv_crypt);
+	printf("Rank %d, Size %d, Crypt shard %d\n", rank, size, crypt_shard);
+
+	MPI_Allgather(&crypt_shard, 1, MPI_INT, crypt, 1, MPI_INT, MPI_COMM_WORLD);
+	printf("Local Crypt: ");
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d ", crypt[i]);
 	}
+	printf("\n\n");
     MPI_Finalize();
 }
-
-//
